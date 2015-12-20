2965811a9954000cfdb05ce865f11c7e78f7f45a
MyWayToJavaJunior <hyperjava@mail.ru>
2015-12-20 17:53:02 +0600

    case done

---
+++ L2.1.2 Authorization/src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,73 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author v.chibrikov
+ *         <p>
+ *         Пример кода для курса на https://stepic.org/
+ *         <p>
+ *         Описание курса и лицензия: https://github.com/vitaly-chibrikov/stepic_java_webserver
+ */
+public class SignUpServlet extends HttpServlet {
+    @SuppressWarnings({"FieldCanBeLocal", "UnusedDeclaration"}) //todo: remove after module 2 home work
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    //get public user profile
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+
+        if (login == null || pass == null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().print("Empty login or password");
+            return;
+        }
+
+        UserProfile profile = accountService.getUserByLogin(login);
+        if (profile != null) {
+            response.setContentType("text/html;charset=utf-8");
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.getWriter().print("User already registered");
+            return;
+        }
+
+        response.setContentType("text/html;charset=utf-8");
+        UserProfile newProfile = new UserProfile(login, pass);
+        accountService.addNewUser(newProfile);
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().print("User created");
+    }
+
+    //change profile
+    public void doPut(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+
+    //unregister
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+        //todo: module 2 home work
+    }
+}
